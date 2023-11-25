2a24e55a0675150470841be0b7cfafba5df11161
Ahmad Shah <ahmad1911491@gmail.com>
2023-11-25 20:40:22 +0500

    Data Ingestion completed

---
+++ main.py
@@ -0,0 +1,14 @@
+from CDC import logger
+from CDC.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = 'Data Ingestion stage'
+try:
+    logger.info(f'>>>>>> Stage {STAGE_NAME} started! <<<<<<')
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(
+        f'>>>>>> Stage {STAGE_NAME} completed! <<<<<<\n\nx=============x')
+except Exception as e:
+    logger.exception(e)
+    raise e
